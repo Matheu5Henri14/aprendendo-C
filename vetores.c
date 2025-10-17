99dafebd5e43c488c56eabc24a8998b76b0bf3a1
Matheus <math.evaristo@gmail.com>
2025-10-17 18:46:42 -0300

    vetores

--- vetores.c
+++ vetores.c
@@ -1,1 +1,38 @@
 #include <stdio.h>
+#include <stddef.h>
+
+
+/*int main(){
+    char nome[15]="algoritmos C";
+    printf("Nome como String: %s\n", nome);
+    printf("Nome caractere por caractere: \n");
+
+    for (int i = 0; nome[i] !='\0'; i ++)
+    {
+        printf("Char na pos.[%d]:%c\n",i,nome[i]);
+    }
+
+}*/
+
+/*int main(){
+    int i=0, aluno,n=0;
+    double soma;
+    printf("Quantos pessoas tem dentro da sala: ");
+    scanf("%d", &aluno);
+    double notas[aluno];
+    for (int i = 0; i < aluno; i++)
+    {
+        printf("Digite as notas dos alunos: ");
+        scanf("%lf",&notas[i]);
+
+        soma+=notas[i];
+
+    }
+
+    printf("O resultado das notasn eh: %2.lf", (soma/aluno));
+ 
+
+    return 0;
+}*/
+
+
